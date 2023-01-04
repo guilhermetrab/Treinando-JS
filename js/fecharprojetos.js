02f950f8c15035e9d3bc65ec365660907d732024
Gui Developer <108558971+guilhermetrab@users.noreply.github.com>
2023-01-04 15:31:37 -0300

    ,

---
+++ js/fecharprojetos.js
@@ -0,0 +1,7 @@
+function fecharprojetos() {
+   if(projetos.style.display == 'none') {
+        projetos.style.display = 'block'
+    } else {
+        projetos.style.display = 'none'
+    }
+}
---
+++ js/openProject.js
@@ -0,0 +1,8 @@
+function openProject() {
+    if(projetos.style.display == 'block') {
+        projetos.style.display = 'none'
+    } else {
+        projetos.style.display = 'block'
+        
+    }
+}
